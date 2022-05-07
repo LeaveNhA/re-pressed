dfa33c3c73f761920670c7f5f1faac7ed311f847
Seçkin KÜKRER <seckin.kukrer@hotmail.com>
2022-05-07 15:30:47 +0300

    Implement prefix on over re-frame events and subs.

--- src/main/re_pressed/core.cljs
+++ src/main/re_pressed/core.cljs
@@ -1,10 +1,7 @@
 (ns re-pressed.core
   (:require
    [re-frame.core :as rf]
-   [re-pressed.impl]
-   ))
-
-
+   [re-pressed.impl]))
 
 (rf/reg-event-fx
  ::add-keyboard-event-listener
@@ -15,49 +12,50 @@
  (fn [_ [_ event-type & {:as args-map}]]
    {::keyboard-event {:event-type event-type :arguments args-map}}))
 
-
 (rf/reg-event-fx
- ::set-keydown-rules
- (fn [{:keys [db]}
-      [_ {:keys [event-keys
-                 clear-keys
-                 always-listen-keys
-                 prevent-default-keys]
-          :as   opts}]]
-   {:db (-> db
-            (assoc-in [::keydown :keys] nil)
-            (assoc-in [::keydown :event-keys] event-keys)
-            (assoc-in [::keydown :clear-keys] clear-keys)
-            (assoc-in [::keydown :always-listen-keys] always-listen-keys)
-            (assoc-in [::keydown :prevent-default-keys] prevent-default-keys)
-            )}))
+    ::set-keydown-rules
+    (fn [{:keys [db]}
+         [_ {:keys [prefix
+                    event-keys
+                    clear-keys
+                    always-listen-keys
+                    prevent-default-keys]
+             :as   opts}]]
+      {:db (-> db
+               (assoc-in [prefix ::keydown :keys] nil)
+               (assoc-in [prefix ::keydown :event-keys] event-keys)
+               (assoc-in [prefix ::keydown :clear-keys] clear-keys)
+               (assoc-in [prefix ::keydown :always-listen-keys] always-listen-keys)
+               (assoc-in [prefix ::keydown :prevent-default-keys] prevent-default-keys)
+               )}))
 
 
-(rf/reg-event-fx
- ::set-keypress-rules
- (fn [{:keys [db]}
-      [_ {:keys [event-keys
-                 clear-keys
-                 always-listen-keys]
-          :as   opts}]]
-   {:db (-> db
-            (assoc-in [::keypress :keys] nil)
-            (assoc-in [::keypress :event-keys] event-keys)
-            (assoc-in [::keypress :clear-keys] clear-keys)
-            (assoc-in [::keypress :always-listen-keys] always-listen-keys)
-            )}))
+   (rf/reg-event-fx
+    ::set-keypress-rules
+    (fn [{:keys [db]}
+         [_ {:keys [prefix
+                    event-keys
+                    clear-keys
+                    always-listen-keys]
+             :as   opts}]]
+      {:db (-> db
+               (assoc-in [prefix ::keypress :keys] nil)
+               (assoc-in [prefix ::keypress :event-keys] event-keys)
+               (assoc-in [prefix ::keypress :clear-keys] clear-keys)
+               (assoc-in [prefix ::keypress :always-listen-keys] always-listen-keys)
+               )}))
 
 
-(rf/reg-event-fx
- ::set-keyup-rules
- (fn [{:keys [db]}
-      [_ {:keys [event-keys
-                 clear-keys
-                 always-listen-keys]
-          :as   opts}]]
-   {:db (-> db
-            (assoc-in [::keyup :keys] nil)
-            (assoc-in [::keyup :event-keys] event-keys)
-            (assoc-in [::keyup :clear-keys] clear-keys)
-            (assoc-in [::keyup :always-listen-keys] always-listen-keys)
-            )}))
+   (rf/reg-event-fx
+    ::set-keyup-rules
+    (fn [{:keys [db]}
+         [_ {:keys [prefix
+                    event-keys
+                    clear-keys
+                    always-listen-keys]
+             :as   opts}]]
+      {:db (-> db
+               (assoc-in [prefix ::keyup :keys] nil)
+               (assoc-in [prefix ::keyup :event-keys] event-keys)
+               (assoc-in [prefix ::keyup :clear-keys] clear-keys)
+               (assoc-in [prefix ::keyup :always-listen-keys] always-listen-keys))}))
